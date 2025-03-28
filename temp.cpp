899088a8cb16bc9ff16807c7b0005f2360c0c48d
Carlos-UCH <carlosbrasil254@gmail.com>
2025-03-28 09:59:24 -0300

    Template

---
+++ temp.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vpll vector<pair<ll, ll>>
+#define endl '\n'
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+
+        
+
+return 0;
+}
